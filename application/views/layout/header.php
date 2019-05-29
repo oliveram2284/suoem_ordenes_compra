837cbfd776bd28d5c137c1a9e53275f4b8abea08
Mauricio Olivera <feboxp@gmail.com>
2019-05-29 11:26:27 -0300

    Cambio de titulo head title

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -4,7 +4,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>FASTraM</title>    
+        <title>SUOEM - Validación de Ordenes de Compra</title>    
         <!-- Bootstrap-->
         <link href="<?php  echo base_url();?>assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
         <!--Common Plugins CSS -->
